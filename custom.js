2662d40f68562b8a2c59a63b6b12f96d12dbd43c
David Vidal <David.a.vidal@hotmail.com>
2023-07-11 17:55:57 -0400

    last commit

---
+++ custom.js
@@ -0,0 +1,13 @@
+// DOESNT WORK CHECK HOME COMPONENT USE EFFECT
+
+/*const Custom = () => {
+  "use strict";
+  new CircleType(document.getElementById("circle-type"));
+};
+Custom();*/
+
+(function () {
+  "use strict";
+
+  new CircleType(document.getElementById("circle-type"));
+});
